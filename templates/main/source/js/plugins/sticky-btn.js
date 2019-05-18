7ce58c9fd1a3d776b7d0a77d2148b3b3efc83e80
Хайруллин Айрат Ильдусович <hairullin.a@tts.ru>
2019-05-18 17:31:49 +0300

    fix

---
+++ templates/main/source/js/plugins/sticky-btn.js
@@ -0,0 +1,68 @@
+(function () {
+	
+	var clientWidth = document.documentElement.clientWidth;
+	if (clientWidth < 768) {
+		
+		var mainSection = document.querySelector('#js-main-section'),
+			winH = window.document.documentElement.clientHeight - 50;
+		
+		var a = document.querySelector('#js-sticky-btn'),
+			b = null,
+			P; // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
+		
+		var scrollPos = 0;
+		$(window).scroll(function(){
+			var st = $(this).scrollTop();
+			if (st > scrollPos){
+				P = winH - 50; //down
+			} else {
+				P = winH; //up
+			}
+			scrollPos = st;
+		});
+		
+		window.addEventListener('scroll', Ascroll, false);
+		document.body.addEventListener('scroll', Ascroll, false);
+		
+		function Ascroll() {
+			if (b == null) {
+				var Sa = getComputedStyle(a, ''), s = '';
+				for (var i = 0; i < Sa.length; i++) {
+					if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+						s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+					}
+				}
+				b = document.createElement('div');
+				b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+				a.insertBefore(b, a.firstChild);
+				var l = a.childNodes.length;
+				for (var i = 1; i < l; i++) {
+					b.appendChild(a.childNodes[1]);
+				}
+				a.style.height = b.getBoundingClientRect().height + 'px';
+				a.style.padding = '0';
+				a.style.border = '0';
+			}
+			var Ra = a.getBoundingClientRect(),
+				R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('#js-indicators').getBoundingClientRect().top + 30);
+			// селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+			if ((Ra.top - P) <= 0) {
+				if ((Ra.top - P) <= R) {
+					b.className = 'stop';
+					b.style.top = -R + 'px';
+				} else {
+					b.className = 'sticky';
+					b.style.top = P + 'px';
+				}
+			} else {
+				b.className = '';
+				b.style.top = '';
+			}
+			window.addEventListener('resize', function () {
+				a.children[0].style.width = getComputedStyle(a, '').width;
+				winH = window.document.documentElement.clientHeight - 50;
+			}, false);
+		}
+		
+	}
+})();
--- templates/main/source/js/users/script.js
+++ templates/main/source/js/users/script.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
-	
+
+	/* Popup
+	======================================== */
 	window.globalPopup = new Popup();
-	
-	$('#js-hamburger').click(function () {
-		
-		$(this).toggleClass('active');
-		$('.js-mobile-menu').toggleClass('active');
-		
-		$('html, body').toggleClass('body-fixed')
-	});
 	
 	$(document).on('click', '[data-ajax]', function (e) {
 		e.stopPropagation();
@@ -18,6 +12,17 @@ $(document).ready(function () {
 		});
 	});
 	
+	/* Mobile menu
+======================================== */
+	$('#js-hamburger').click(function () {
+		
+		$(this).toggleClass('active');
+		$('.js-mobile-menu').toggleClass('active');
+		$('html, body').toggleClass('body-fixed')
+	});
+	
+	/* Main section Swiper
+	======================================== */
 	var mainSwiper = new Swiper('.js-main-slider .swiper-container', {
 		speed: 1500,
 		slidesPerView: 1,
@@ -29,12 +34,10 @@ $(document).ready(function () {
 		pagination: {
 			el: '.swiper-pagination',
 			type: 'fraction'
-			
-			
 		},
 		navigation: {
 			nextEl: '.js-main-slider-next',
-			prevEl: '.js-main-slider-prev',
+			prevEl: '.js-main-slider-prev'
 		},
 		breakpoints: {
 			
@@ -42,127 +45,64 @@ $(document).ready(function () {
 				pagination: {
 					el: '.swiper-pagination',
 					type: 'bullets'
-					
-					
-				},
+				}
 			}
 		}
 	});
-	
-	var stickyBtn = $('#js-sticky-btn'),
-		mainPage = $('#js-main-section'),
-		mainPageH = mainPage.height(),
-		stickyBtnHeight = stickyBtn.height(),
-		finalSection = $('#js-indicators'),
-		btnOffsetTop = stickyBtn.offset().top,
-		target = finalSection.offset().top - 200;
-	
-	$('.test').css('top', finalSection.offset().top - 110);
-	console.log(stickyBtnHeight, 'stickyBtnHeight');
-	
-	$(document).on('scroll', function () {
-		var documentScroll = $(this).scrollTop(),
-			documentScrollBottom = documentScroll + mainPageH + stickyBtnHeight + 15;
-		
-		// console.log(documentScroll, 'documentScroll');
-		// console.log(btnOffsetTop, 'btnOffsetTop');
-		
-		
-		console.log(documentScrollBottom, 'documentScrollBottom');
-		
-		console.log(target, 'target');
-		
-		
-		// if (documentScrollBottom > mainPageH + 180) {
-		if (btnOffsetTop < documentScrollBottom && documentScrollBottom < target) {
-			stickyBtn.addClass('sticky');
-			mainPage.addClass('sticky');
-			stickyBtn.removeClass('sticky-end');
-			mainPage.removeClass('sticky-end');
-		} else if (documentScrollBottom > target){
-			
-			console.log('sticky-end');
-			stickyBtn.removeClass('sticky');
-			mainPage.removeClass('sticky');
-			stickyBtn.addClass('sticky-end');
-			mainPage.addClass('sticky-end');
-			// $('.sticky-end').css('top', target - 200 + 'px');
-		} else {
-			stickyBtn.removeClass('sticky');
-			mainPage.removeClass('sticky');
-			stickyBtn.removeClass('sticky-end');
-			mainPage.removeClass('sticky-end');
-		}
-		
-		// if (documentScroll > stickyBtnPos) {
-		//
-		// 	stickyBtn.addClass('fixed');
-		// 	stickyBtn.css('top', stickyBtnH);
-		// } else {
-		//
-		// 	stickyBtn.removeClass('fixed');
-		// 	stickyBtn.css('top', stickyBtnPos + menuH);
-		// }
-	});
-	
-	var tabsMenuSwiper = new Swiper('.js-tabs-menu-slider', {
-		slidesPerView: 5,
-		slidesPerGroup: 1,
-		speed: 800,
-		spaceBetween: 30,
-		navigation: {
-			nextEl: '.js-tabs-menu-next',
-			prevEl: '.js-tabs-menu-prev',
-		},
-		breakpoints: {
-			1200: {
-				slidesPerView: 4,
-				spaceBetween: 30
+
+	/* Tabs Swiper
+	======================================== */
+	var clientWidthTabs = document.documentElement.clientWidth;
+	if (clientWidthTabs > 768) {
+		var tabsMenuSwiper = new Swiper('.js-tabs-menu-slider', {
+			slidesPerView: 5,
+			slidesPerGroup: 1,
+			speed: 800,
+			spaceBetween: 30,
+			navigation: {
+				nextEl: '.js-tabs-menu-next',
+				prevEl: '.js-tabs-menu-prev'
 			},
-			992: {
-				slidesPerView: 3,
-				spaceBetween: 34
+			breakpoints: {
+				1200: {
+					slidesPerView: 4,
+					spaceBetween: 30
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 34
+				}
 			}
-		}
-	});
+		});
+	}
 	
-	// (function () {
-	// 	var mainPageH = $('#js-main-section').height();
-	// 	var stickyBtn = $('#js-sticky-btn');
-	//
-	// 		$(document).on('scroll', function () {
-	//
-	// 			var stickyBtnTop = stickyBtn.offset().top,
-	// 				documentScroll = $(this).scrollTop() + stickyBtnTop;
-	//
-	// 			console.log(stickyBtnTop, 'btn offset top');
-	// 			console.log(documentScroll, 'document scroll');
-	//
-	// 			if (documentScroll < stickyBtnTop) {
-	//
-	// 				stickyBtn.addClass('fixed');
-	// 			}
-	// 		});
-	// })();
-	
-	
+	/* Phone mask
+	======================================== */
+
 	$("[type=tel]").inputmask("(999) 999-99-99");
 	
-	
+	/* All circles
+	======================================== */
 	circles();
 	
+	/* Tabs + refresh circles
+	======================================== */
 	new Tabs({
 		calcbackFunc: function () {
 			circles();
 		}
 	});
 	
+	/* Tooltip
+	======================================== */
 	var tooltip = new Tooltip('tooltip');
 	
+	/* Smoth scroll to section
+	======================================== */
 	$(".js-anim-scroll").click(function (e) {
 		e.preventDefault();
 		
-		var offset = parseInt(this.getAttribute('data-offset')) || 150;
+		var offset = parseInt(this.getAttribute('data-offset')) || 90;
 		
 		var top = $(this.getAttribute("href")).offset().top - offset;
 		
@@ -181,6 +121,8 @@ $(document).ready(function () {
 		} else if ($(this).attr('href') == '#js-platforms-tabs-6') {
 			$('.platforms__tabs .tabs-menu').children()[5].click();
 		}
+		
+		$('#js-hamburger.active').click();
 		
 		return false;
 		
